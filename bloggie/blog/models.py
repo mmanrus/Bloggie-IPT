c9a8dec15cdd18543bc011bb09e6ae25418b53c1
mmanrus <mmanrusiana@gmail.com>
2024-04-11 11:54:30 +0800

    taking changing date_posted model to model.DateTime() now orders by date published

--- bloggie/blog/models.py
+++ bloggie/blog/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-
+from datetime import datetime, date
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255, default='')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_posted = models.DateTimeField(auto_now_add=True)
+    date_posted = models.DateField(auto_now_add=True)
     body = models.TextField()
     
     def __str__(self):
-        return self.title + ' by ' + str(self.author) + '      ' + str(self.date_posted)
+        return self.title + ' by ' + str(self.author) + ' Date Published: ' + str(self.date_posted)
     
     def get_absolute_url(self):
         return reverse("article-detail", args=(str(self.id)))
--- bloggie/blog/views.py
+++ bloggie/blog/views.py
@@ -11,6 +11,7 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post
     template_name = 'blog/home.html'
+    ordering = ['-date_posted']
     
 class ArticleDetail(DetailView):
     model = Post
